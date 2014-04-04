03d21727301e37a21b99590c0c77db770ce86d0c
Greg Palmier <gregpalmier@gmail.com>
2014-04-04 12:45:35 -0500

    version bump, want to start integration.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'gregpalmier@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures promet_nginx-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 depends		 'apt'
 depends		 'mysql'
 depends		 'php'
